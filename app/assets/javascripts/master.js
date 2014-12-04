518feb016d227aa1b07bccc0e5c84c33ef1dd9af
Jose Moreno <jose.moreno@reyesic.net>
2014-12-04 18:27:08 +0000

    Basic JS case

---
+++ app/assets/javascripts/master.js
@@ -0,0 +1,27 @@
+function isInt(value) {
+    var val = value.val();
+    if(Math.floor(val) == val && $.isNumeric(val)) {
+        return parseInt(val);
+    } else {
+        return false;
+    }
+}
+
+$(document).ready(function(){
+    $('.ops').on("click",'button',function(){
+        var x, y, z;
+        x = isInt($('.x'))
+        y = isInt($('.y'))
+        
+        $('.X').text(x);
+        $('.Y').text(y);
+        
+        if($(this).hasClass("plus")) {
+            z = x + y
+            $('.Z').text(z);
+        } else {
+            z = x - y
+            $('.Z').text(z);
+        }
+    });
+});
